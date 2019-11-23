f9278d032ef22511458f58f4e4534000c1e2f52a
赵进召 <zhaojinzhao@zhaojinzhaodeMacBook-Pro.local>
2019-11-23 14:32:50 +0800

    feat(headers): 增加自定义header的能力

--- examples/base/app.ts
+++ examples/base/app.ts
@@ -67,3 +67,40 @@ axios({
         bar: 'baz'
     }
 })
+
+axios({
+    method: 'post',
+    url: '/base/post',
+    data: {
+        a: 1,
+        b: 2
+    }
+})
+
+const arr = new Int32Array([21, 31]);
+axios({
+    method: 'post',
+    url: '/base/buffer',
+    data: arr
+})
+
+axios({
+    method: 'post',
+    url: '/base/post',
+    data: {
+        a: 1,
+        b: 2
+    },
+    headers: {
+        'content-type': 'application/json',
+        'Accept': 'application/json, text/plain, */*'
+    }
+})
+
+const paramString = 'q=URLUtils.searchParams&topic=api';
+const searchParams = new URLSearchParams(paramString);
+axios({
+    method: 'post',
+    url: '/base/post',
+    data: searchParams
+})
--- src/xhr.ts
+++ src/xhr.ts
@@ -1,9 +1,16 @@
 import { AxiosRequestConfig } from './types/index'
 
 export default function xhr(config: AxiosRequestConfig): void {
-  const { data = null, url, params, method = 'get' } = config
+  const { data = null, url, method = 'get', headers } = config
 
   const request = new XMLHttpRequest()
   request.open(method.toUpperCase(), url, true)
+
+  Object.keys(headers).forEach(name => {
+    if (data === null && name.toLowerCase() === 'content-type') {
+      return
+    }
+    request.setRequestHeader(name, headers[name])
+  })
   request.send(data)
 }
